fc999cff9a93223c211dc7560cc36f0671f21a6c
BuildTools <unconfigured@null.spigotmc.org>
2020-07-05 17:04:32 -0500

    Implemented all other pages.

--- src/app.js
+++ src/app.js
@@ -14,9 +14,45 @@ app.set("views", viewsDir)
 app.use(express.static(publicDir))
 hbs.registerPartials(partialsDir)
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
     res.render("index", {
 
+    })
+})
+
+app.get("/chemistry", (req, res) => {
+    res.render("index", {
+
+    })
+})
+
+app.get("/life-sciences", (req, res) => {
+    res.render("life-sciences", {
+
+    })
+})
+
+app.get("/earth-sciences", (req, res) => {
+    res.render("earth-sciences", {
+
+    })
+})
+
+app.get("/physics", (req, res) => {
+    res.render("physics", {
+
+    })
+})
+
+app.get("/technology", (req, res) => {
+    res.render("technology", {
+
+    })
+})
+
+app.get("*", (req, res) => {
+    res.render("error", {
+        
     })
 })
 
